1a0540d84b5bd0bc894cf5e7e91b65eeb6e952da
HinanawiTenko2 <eightyhare@gmail.com>
2023-05-09 11:19:01 -0600

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -11,22 +11,39 @@ channelF.close()
 local bot = createBot(token)
 local systemME = peripheral.find('meBridge')
 
-local oldID = nil
+local messages = bot.getMessages(channel)
+local oldID = messages[1].id
 
 
 while true do
-    local messages = bot.getMessages(channel)
+    messages = bot.getMessages(channel)
     if not(messages[1].id == oldID) then
        local message = messages[1].content
        if string.find(message, '!getCount') then
             local item = string.sub(message, 11)
             local MEItems = systemME.listItems()
+            local found = false
             for _,i in pairs(MEItems) do
                 if i.name == item then
                     bot.send('There is ' .. tostring(i.amount) .. ' ' .. item .. ' in the ME system', channel)
+                    found = true
                 end
             end
+            if not found then
+                print(item .. ' was not found in the system')
+            end
        end 
+       if string.find(message, '!dumpME') then
+            local dump = ''
+            local MEItems = systemME.listItems()
+            for _,i in pairs(MEItems) do
+                dump = dump .. i.name .. '\n'
+                if string.len(dump) > 1500 then
+                    bot.send(dump, channel)
+                    dump = ''
+                end
+            end
+       end
     end
     oldID = messages[1].id
     sleep(1)
